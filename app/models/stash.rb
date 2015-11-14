cade3d19a0455454a59674f9b10bb415e56b6cce
Carl Westmoreland <beguira86@gmail.com>
2015-11-14 11:37:15 -0500

    minor changes to fix paperclip and s3

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -1,4 +1,8 @@
 class Stash < ActiveRecord::Base
 	belongs_to :user
 	has_many :games
+
+	has_attached_file :image
+	validates :image, attachment_presence: true
+	
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ config.paperclip_defaults = {
   s3_credentials: {
       access_key_id: ENV["S3_ACCESS_KEY"],
       secret_access_key: ENV["S3_SECRET_KEY"],
-      bucket: ENV["S3_TEST_BUCKET"]
+      bucket: ENV["S3_BUCKET"]
     }
   }
 end
