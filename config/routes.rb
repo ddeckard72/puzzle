b6e738501bfe9199a0dd1b44c7a6450ea4cb0b5b
Don Deckard <don_deckard@yahoo.com>
2015-11-14 11:51:18 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,10 @@ resources :stash
 #            PATCH  /stash/:id(.:format)      stash#update
 #            PUT    /stash/:id(.:format)      stash#update
 #            DELETE /stash/:id(.:format)      stash#destroy
+
+#game routes
+get "game/puzzle/:id" , to: "game#index"
+post "game/puzzle/:id" , to: "game#save"
+#            GET    /game/puzzle/:id(.:format) game#index
+#            POST   /game/puzzle/:id(.:format) game#save
 end
