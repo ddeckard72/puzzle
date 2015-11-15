c0be74a9bf5dda4933b905d9b4405b1e1ee4ea4b
Carl Westmoreland <beguira86@gmail.com>
2015-11-15 09:30:36 -0500

    another minor change to json

--- app/views/stash/index.json.jbuilder
+++ app/views/stash/index.json.jbuilder
@@ -1,4 +1,7 @@
 json.puzzles @puzzles do |puzzle|
   json.about puzzle.about
   json.image puzzle.image
+  json.title puzzle.title
+  json.level puzzle.level
+  json.author puzzle.author
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115035434) do
+ActiveRecord::Schema.define(version: 20151115142202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20151115035434) do
     t.datetime "image_updated_at"
     t.string   "level"
     t.integer  "user_id"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
