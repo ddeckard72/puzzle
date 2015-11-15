ad65d4037788c49fe0a34816f415385c933f3ff4
Carl Westmoreland <beguira86@gmail.com>
2015-11-14 21:57:06 -0500

    fix typo

--- app/controllers/stash_controller.rb
+++ app/controllers/stash_controller.rb
@@ -1,6 +1,6 @@
 #puzzle controller
 class StashController < ActionController::Base
-	before_action :authenticate_user!, except [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
 
 
 	def index
