2b096c520cd53d57aa2c1979489efba08cb125cf
Carl Westmoreland <beguira86@gmail.com>
2015-11-14 20:09:16 -0500

    add puma back to gemfile, and new migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114222312) do
+ActiveRecord::Schema.define(version: 20151115010618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20151114222312) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "access_token"
   end
 
 end
