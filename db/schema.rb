7c1136f19473db4de1fe53afdcde720409a0fed7
Carl Westmoreland <beguira86@gmail.com>
2015-11-15 08:38:55 -0500

    heroku rake to add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151115010618) do
+ActiveRecord::Schema.define(version: 20151115035434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20151115010618) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.string   "level"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
